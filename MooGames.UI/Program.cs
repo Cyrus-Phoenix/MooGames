4b768cee1870157c4573ee5157fe583c15915f4b
Reza Shahbazi aka Cyrus Phoenix <mreza.shahbazi@live.com>
2024-08-09 11:44:47 +0200

    added blank space

--- MooGames.UI/Program.cs
+++ MooGames.UI/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,Happy World!");
+            Console.WriteLine("Hello, Happy World!");
         }
     }
 }

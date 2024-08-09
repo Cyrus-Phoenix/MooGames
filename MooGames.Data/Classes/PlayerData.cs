ffb480f82dadefb249caa659966844c01405c102
Reza Shahbazi aka Cyrus Phoenix <mreza.shahbazi@live.com>
2024-08-09 13:14:33 +0200

    start refactoring codebase

---
+++ MooGames.Data/Classes/PlayerData.cs
@@ -0,0 +1,39 @@
+﻿using MooGames.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MooGames.Data.Classes
+{
+    internal class PlayerData : IPlayerData
+    {
+        public string Name { get; private set; }
+        public int NGames { get; private set; }
+        
+        int totalGuess;
+
+        public void Update(int guesses)
+        {
+            totalGuess += guesses;
+            NGames++;
+        }
+
+        public double Average()
+        {
+            return (double)totalGuess / NGames;
+        }
+
+        public override bool Equals(Object p)
+        {
+            return Name.Equals(((PlayerData)p).Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+    }
+}
--- MooGames.Data/Data.cs
+++ MooGames.Data/Data.cs
@@ -1,7 +1,15 @@
-﻿namespace MooGames.Data
+﻿using MooGames.Data.Classes;
+
+namespace MooGames.Data
 {
     public class Data
     {
+        private readonly PlayerData playerData;
 
+        public Data()
+        {
+            
+        }
     }
+    
 }
